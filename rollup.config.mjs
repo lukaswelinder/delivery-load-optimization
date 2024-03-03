83fa409a8565cea9d876d9149a28b41be9915b8e
Lukas Welinder <Lukas@Welinder.Co>
2024-03-02 17:54:52 -0700

    chore: initial project setup

---
+++ rollup.config.mjs
@@ -0,0 +1,10 @@
+import esbuild from 'rollup-plugin-esbuild';
+
+export default {
+  plugins: [esbuild()],
+  input: 'src/index.ts',
+  output: {
+    file: 'dist/bundle.js',
+    format: 'cjs',
+  },
+};
